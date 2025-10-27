84eeec7dda4ad4f7f3229cfb13d1e893fdec89e8
dmc0125 <dominikmichal0124@gmail.com>
2025-10-27 10:18:49 +0100

    modify colors

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -1,16 +1,30 @@
 vim.cmd("colorscheme default")
 
-vim.api.nvim_set_hl(0, "@string", { fg = "#74d4ff" })
+local colors = {
+    sky_blue = "#74d4ff",
+    pink = "#f6339a",
+    orange = "#ff7e57",
+    grey = "#bdc4cd",
+    white = "#e3eaf4",
+    dark_grey = "#626870",
+}
 
-vim.api.nvim_set_hl(0, "@function", { fg = "#f6339a" })
-vim.api.nvim_set_hl(0, "@type", { fg = "#f6339a" })
-vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#f6339a" })
+vim.api.nvim_set_hl(0, "@string", { fg = colors.sky_blue, force = true })
+vim.api.nvim_set_hl(0, "@string.regexp", { fg = colors.sky_blue, force = true })
 
-vim.api.nvim_set_hl(0, "@constant", { fg = "#ff7e57" })
+vim.api.nvim_set_hl(0, "@function", { fg = colors.pink })
+vim.api.nvim_set_hl(0, "@type", { fg = colors.pink })
+vim.api.nvim_set_hl(0, "@type.builtin", { fg = colors.pink })
 
-vim.api.nvim_set_hl(0, "@keyword", { fg = "#bdc4cd" })
-vim.api.nvim_set_hl(0, "@variable.member", { fg = "#bdc4cd" })
+vim.api.nvim_set_hl(0, "@constant", { fg = colors.orange })
 
-vim.api.nvim_set_hl(0, "@variable", { fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "@keyword", { fg = colors.grey })
+vim.api.nvim_set_hl(0, "@variable.member", { fg = colors.grey })
+vim.api.nvim_set_hl(0, "@property", { fg = colors.grey })
 
-vim.api.nvim_set_hl(0, "@comment", { fg = "#626870" })
+vim.api.nvim_set_hl(0, "@variable", { fg = colors.white })
+
+vim.api.nvim_set_hl(0, "@comment", { fg = colors.dark_grey })
+
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = colors.dark_grey })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = colors.dark_grey })
